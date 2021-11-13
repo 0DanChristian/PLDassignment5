af15d3dc7c090a1c9fbedb36b6a70090e658da5a
Dan Christian <dansilog16@gmail.com>
2021-11-13 20:11:06 +0545

    allset

--- lowestno.py
+++ lowestno.py
@@ -2,10 +2,21 @@
 # find the lowest number 
 
 # ask for the first number
-first_number = int(input("Enter your first desired number: "))
+first_no = int(input("Enter your first desired number: "))
 # ask for the second number 
-second_number = int(input("Enter your second desired number: "))
+second_no = int(input("Enter your second desired number: "))
 # ask for the third number
-third_number = int(input("Enter your third desired number: "))
+third_no = int(input("Enter your third desired number: "))
 # what number is the smallest
 smallest = 0
+# your statement
+if first_no < second_no and first_no < third_no :
+    smallest = first_no
+if second_no < first_no and second_no < third_no :
+    smallest = second_no
+if third_no < first_no and third_no < second_no :
+    smallest = third_no
+# display
+print(smallest, "is the smallest of your three desired numbers.")
+
+# neat
