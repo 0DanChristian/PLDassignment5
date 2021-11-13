e0e2a170ccc081e0c59d2c582d4aa6e58052b9ea
Dan Christian <dansilog16@gmail.com>
2021-11-13 19:05:17 +0545

    gradingSystem

---
+++ grade.py
@@ -0,0 +1,14 @@
+# program #2: grading
+# grade/mark    percentage     description
+# 1.0           97-100          Excellent
+# 1.25          94-96           Excellent
+# 1.5           91-93           Very Good
+# 1.75          88-90           Very Good
+# 2.0           85-87           Good
+# 2.25          82-84           Good
+# 2.75          76-78           Satisfactory
+# 3.0           75              Passing
+# 5.0           65-74           Failure
+# Inc.                          Incomplete
+# W                             Withdrawn
+# D                             Dropeed
