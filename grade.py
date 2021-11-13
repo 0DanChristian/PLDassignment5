470d0bd75c7ee08975200adf4f43c5005c2a92d4
Dan Christian <dansilog16@gmail.com>
2021-11-14 01:22:11 +0545

    float

--- grade.py
+++ grade.py
@@ -14,26 +14,27 @@
 # D                             Dropeed
 
 import math
-grade = float(input("Please enter your grade: "))
+grade = int(float(input('Please enter your grade: ').replace('.2%', '')))
+
 # Uno
-if grade >= 97 and grade <= 100:
+if grade >= 97.00 and grade <= 100.00:
     print("1.0 is your grade! Excellent!")
-elif grade >= 94 and grade <= 96:
-    print("1.25 is your grade! Very Good!")
-elif grade >= 91 and grade <= 93:
+elif grade >= 94.00 and grade <= 96.00:
+    print("1.25 is your grade! Excellent!")
+elif grade >= 91.00 and grade <= 93.00:
     print("1.5 is your grade! Very Good")
-elif grade >= 88 and grade <= 90:
+elif grade >= 88.00 and grade <= 90.00:
     print("1.75 is your grade! Very Good!") 
-elif grade >= 85 and grade <= 87:
+elif grade >= 85.00 and grade <= 87.00:
     print("2.0 is your grade! It was good! keep it up ")
-elif grade >= 82 and grade <= 84:
+elif grade >= 82.00 and grade <= 84.00:
     print("2.25 is your grade! It was good! keep it up!")
-elif grade >= 76 and grade <= 78:
+elif grade >= 76.00 and grade <= 78.00:
     print("2.75 is your grade! Satisfactory, keep it up buddy!")
-elif grade >= 65 and grade <= 74:
+elif grade >= 65.00 and grade <= 74.00:
     print("Unfortunately, you failed. Your grade is 5.0")
 
-elif grade <= 75:
+elif grade <= 75.00:
     print("Your grade is 3.0, its passing. Keep it up! hard work pays off")
 
 
