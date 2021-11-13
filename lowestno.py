a2bbccd5e38f0ec7e15c359eb183d20607dde91e
Dan Christian <dansilog16@gmail.com>
2021-11-13 20:07:41 +0545

    elif_remaining

--- lowestno.py
+++ lowestno.py
@@ -1,3 +1,11 @@
 # create a program that asks for 3 numebrs
 # find the lowest number 
 
+# ask for the first number
+first_number = int(input("Enter your first desired number: "))
+# ask for the second number 
+second_number = int(input("Enter your second desired number: "))
+# ask for the third number
+third_number = int(input("Enter your third desired number: "))
+# what number is the smallest
+smallest = 0
